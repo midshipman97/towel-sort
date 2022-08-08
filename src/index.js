8d81468100b744c74c136e25f56b23f4af46bd09
MIchael <tmkovver@mail.ru>
2022-08-08 23:17:28 +0700

    feat: complete tast towel-sort

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort (matrix) { //проверяем на заполненность аргумент и нечетные элементы массива (т.к. начинается с "0") реверсим
+  if (!matrix) {
+    return [];
+  }
+  let arr = [];
+  for (let i = 0; i < matrix.length; i++) {
+    if (i % 2 === 0) {
+      arr = arr.concat(matrix[i]);
+    } else {
+      arr = arr.concat(matrix[i].reverse());
+    }
+  }
+  return arr
 }
